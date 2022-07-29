f3e0047bf199228b0b87820b4d4f98853a5067eb
Egor Glebov <batmanvs28@gmail.com>
2022-07-29 10:27:08 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import './index.css';
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+// import divider-mobile from '../public/images/'
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [id, setId] = useState(null);
+
+  const getAdvice = async () => {
+    const response = await fetch(`https://api.adviceslip.com/advice`);
+    const responseJSON = await response.json();
+    return responseJSON.slip;
+  };
+
+  const newAdvice = () => getAdvice().then((data) => {
+      const { id, advice } = data;
+      setAdvice(advice);
+      setId(id);
+      console.log('click');
+    });
+
+  useEffect(() => {
+    newAdvice();
+    console.log("update");
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <div className="card">
+        <p className="advice-id">advice # {id}</p>
+        <h3 className="advice-text">
+          <FaQuoteLeft className="quote" />
+          {advice}
+          <FaQuoteRight className="quote" />
+        </h3>
+        <div className="btn-container">
+          <div className="divider"></div>
+          <button className="btn" onClick={newAdvice}/>
+        </div>
+      </div>
+    </main>
   );
 }
 
